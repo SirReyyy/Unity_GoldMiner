f1b8cebf685388213ab3317fbd0a1886b696ee86
Rey Oronos, Jr <rey.oronos.jr@gmail.com>
2022-10-25 16:23:03 +0800

    25/10/22 - Items cont.
    - Adjusted collider settings to some items
    - Set item colliders as triggers
    - Created prefabs for the items
    - Created new script TagManager
    - Added a child to the Hook on the Player prefab

---
+++ Assets/Scripts/Helper Scripts/TagManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tags {
+
+    public const string ITEM_BARREL = "Item_Barrel";
+
+    public const string ITEM_COINS = "Item_Coins";
+    public const string ITEM_GOLDS = "Item_GoldS";
+    public const string ITEM_GOLDM = "Item_GoldM";
+    public const string ITEM_GOLDL = "Item_GoldL";
+
+    public const string ITEM_RUBY = "Item_Ruby";
+    public const string ITEM_EMERALD = "Item_Emerald";
+
+    public const string ITEM_STONES = "Item_StoneS";
+    public const string ITEM_STONEM = "Item_StoneM";
+
+    public const string ITEM_DELIVERED = "Item_Delivered";
+
+}  // --- Tags Class --- //
+
+public class AnimationTags {
+
+    public const string IDLE_ANIM = "Player_Anim_Idle";
+    public const string CHEER_ANIM = "Player_Anim_Cheer";
+    public const string ROPEWRAP_ANIM = "Player_Anim_RopeWrap";
+
+} // --- AnimationTags Class --- //
